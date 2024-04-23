afbf94af199f268d3e666686dce3f83ac6c7e348
JoasOliveira <167044561+JoasOliveira@users.noreply.github.com>
2024-04-22 22:41:28 -0300

    Update bot.py
    adcionei uma tela de inicialização e botões

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 import keyring
 from discord.ext import commands
-from config import setup_commands
+from discord.ui import View, Button
 
 bot = commands.Bot(command_prefix='>', intents=discord.Intents.all())
 
@@ -14,6 +14,31 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-setup_commands(bot)
+    # Mensagem de inicialização
+    startup_message = (
+        "Olá! Eu sou o seu assistente de leitura.\n"
+        "Estou aqui para ajudá-lo a encontrar livros sobre várias categorias e tópicos.\n"
+        "Por favor, selecione uma ação abaixo:"
+    )
+    
+    # Cria uma view 
+    view = View()
+
+    # Adicione botões à view
+    button = Button(style=discord.ButtonStyle.primary, label="Mostrar Categorias", custom_id="show_categories")
+    button.callback = show_categories_callback  # Atribuindo o callback method
+    view.add_item(button)
+    
+    # Enviar mensagem com os botões no Canal especifico do Bot no Servidor Discord
+    channel = bot.get_channel(1231763443128864832)  # Substitua pelo ID do canal desejado
+    if channel:
+        await channel.send(content=startup_message, view=view)
+    else:
+        print("Canal não encontrado. Certifique-se de substituir o ID do canal pelo correto.")
+
+# Callback method para o botão
+async def show_categories_callback(interaction: discord.Interaction):
+    # Cuide da interação aqui
+    await interaction.response.send_message("As categorias serão mostradas aqui.")
 
 bot.run(keyring.get_password('discord_book', 'token'))
